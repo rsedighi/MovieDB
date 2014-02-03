a1a4e0c3ffc5f432474dbe7b8bddac85035f6dc6
Ramin <rsedighi@gmail.com>
2014-02-03 12:25:42 -0800

    First commit with README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieDB::Application.config.secret_key_base = '8434aa9405a48ea383b955eb62e8297b07cc28725d981e176f90c9ba8215339384d51ff4da0f1aba1080e88d985fbcb62a8657ee5e58815daf98cb54385dcb89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovieDB::Application.config.session_store :cookie_store, key: '_MovieDB_session'
